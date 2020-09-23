f51e7629d4f9de1dff5469b43de0863bb2f6ede9
Matheus-Aguilar <matheusdeoliveira.ti@gmail.com>
2020-09-23 12:28:25 -0300

    Added Interface

--- app/src/main/java/android/teste/matheusaguilar/calculadora2/MainActivity.java
+++ app/src/main/java/android/teste/matheusaguilar/calculadora2/MainActivity.java
@@ -3,6 +3,7 @@ package android.teste.matheusaguilar.calculadora2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void clickNumberButton(View view){
+
+    }
+
+    public void clickOperatorButton(View view){
+
+    }
+
+    public void clickPointButton(View view){
+
+    }
+
+    public void clickEqualButton(View view){
+
+    }
+
+    public void clickBackSpaceButton(View view){
+
+    }
+
+    public void clickClearButton(View view){
+
     }
 }
