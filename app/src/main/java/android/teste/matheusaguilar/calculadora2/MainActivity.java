ad70610bf8b64b317798c502e2bbfcf069dd3417
Matheus-Aguilar <matheusdeoliveira.ti@gmail.com>
2020-09-25 22:54:03 -0300

    Added invalid input treatments

--- app/src/main/java/android/teste/matheusaguilar/calculadora2/MainActivity.java
+++ app/src/main/java/android/teste/matheusaguilar/calculadora2/MainActivity.java
@@ -18,64 +18,100 @@ public class MainActivity extends AppCompatActivity {
     public void clickNumberButton(View view){
         EditText visorView = (EditText) findViewById(R.id.visor);
         TextView errorsView = (TextView) findViewById(R.id.errorsText);
-        String text = visorView.getText().toString();
 
-        char last = '\0';
-
-        if(text.length() > 0)
-            last = text.charAt(text.length() - 1);
-
-        if(last == '+' || last == '-' || last == 'x' || last == '/')
-            visorView.setText(text + ' ' + view.getTag().toString());
-        else
-            visorView.setText(text + view.getTag().toString());
+        String[] parts = visorView.getText().toString().split(" ");
+        String digit = view.getTag().toString();
 
         errorsView.setText("");
+
+
+        if(parts[0].length() == 0)
+            visorView.setText(digit);
+        else if(parts.length == 1)
+            visorView.setText(parts[0] + digit);
+        else if(parts.length == 2)
+            visorView.setText(parts[0] + ' ' + parts[1] + ' ' + digit);
+        else if(parts.length == 3)
+            visorView.setText(parts[0] + ' ' + parts[1] + ' ' + parts[2] + digit);
+        else
+            errorsView.setText("Erro desconhecido");
     }
 
     public void clickOperatorButton(View view){
         EditText visorView = (EditText) findViewById(R.id.visor);
         TextView errorsView = (TextView) findViewById(R.id.errorsText);
-        String text = visorView.getText().toString();
 
-        visorView.setText(text + ' ' + view.getTag().toString());
+        String[] parts = visorView.getText().toString().split(" ");
+        String op = view.getTag().toString();
+
         errorsView.setText("");
+
+        if(parts[0].length() == 0) {
+            if(op.equals("-"))
+                visorView.setText(op);
+        }
+        else if(parts.length == 1) {
+            if(parts[0].charAt(parts[0].length() - 1) != '-' && parts[0].charAt(parts[0].length() - 1) != '.')
+                visorView.setText(parts[0] + ' ' + op);
+        }
+        else if(parts.length == 2) {
+            if(op.equals("-"))
+                visorView.setText(parts[0] + ' ' + parts[1] + ' ' + op);
+        }
+        else if(parts.length == 3)
+            return;
+        else
+            errorsView.setText("Erro desconhecido");
     }
 
     public void clickPointButton(View view){
         EditText visorView = (EditText) findViewById(R.id.visor);
         TextView errorsView = (TextView) findViewById(R.id.errorsText);
-        String text = visorView.getText().toString();
 
-        char last = '\0';
-
-        if(text.length() > 0)
-            last = text.charAt(text.length() - 1);
-
-        if(last == '+' || last == '-' || last == 'x' || last == '/')
-            visorView.setText(text + ' ' + '.');
-        else
-            visorView.setText(text + '.');
+        String[] parts = visorView.getText().toString().split(" ");
 
         errorsView.setText("");
+
+
+        if(parts[0].length() == 0)
+            return;
+        else if(parts.length == 1) {
+            if(parts[0].indexOf('.') == -1 && parts[0].length() > 0)
+                visorView.setText(parts[0] + '.');
+        }
+        else if(parts.length == 2)
+            return;
+        else if(parts.length == 3) {
+            if (parts[2].indexOf('.') == -1)
+                visorView.setText(parts[0] + ' ' + parts[1] + ' ' + parts[2] + '.');
+        }
+        else
+            errorsView.setText("Erro desconhecido");
     }
 
     public void clickEqualButton(View view){
         EditText visorView = (EditText) findViewById(R.id.visor);
         TextView errorsView = (TextView) findViewById(R.id.errorsText);
 
-        String text = visorView.getText().toString();
-        String parts[] = text.split(" ");
+        String[] parts = visorView.getText().toString().split(" ");
+
+        errorsView.setText("");
 
         if(parts.length == 3){
+
+            if(parts[0].charAt(parts[0].length() - 1) == '-' || parts[0].charAt(parts[0].length() - 1) == '.')
+                return;
+
+            if(parts[2].charAt(parts[2].length() - 1) == '-' || parts[2].charAt(parts[2].length() - 1) == '.')
+                return;
+
             double n1, n2;
+
             try{
                 n1 = Double.parseDouble(parts[0]);
                 n2 = Double.parseDouble(parts[2]);
             }
             catch(NumberFormatException e){
-                visorView.setText("");
-                errorsView.setText("Erro: Sintaxa Inválida");
                 return;
             }
 
@@ -88,45 +124,44 @@ public class MainActivity extends AppCompatActivity {
             else if(parts[1].equals("/")){
                 if(n2 != 0.0)
                     visorView.setText(Double.toString(n1 / n2));
-                else{
-                    visorView.setText("");
+                else
                     errorsView.setText("Erro: Divisao por Zero");
-                }
             }
             else{
                 visorView.setText("");
                 errorsView.setText("Erro: Operador Invalido");
             }
         }
-        else{
-            visorView.setText("");
-            errorsView.setText("Erro: Sintaxe Inválida");
-        }
     }
 
     public void clickBackSpaceButton(View view){
+
         EditText visorView = (EditText) findViewById(R.id.visor);
         TextView errorsView = (TextView) findViewById(R.id.errorsText);
-        String text = visorView.getText().toString();
 
-        if(text.length() > 0) {
+        String[] parts = visorView.getText().toString().split(" ");
 
-            char last = text.charAt(text.length() - 1);
-
-            if(last == ' ')
-                visorView.setText(text.substring(0, text.length() - 3));
-            else if(last == '+' || last == '-' || last == 'x' || last == '/')
-                visorView.setText(text.substring(0, text.length() - 2));
-            else
-                visorView.setText(text.substring(0, text.length() - 1));
-        }
         errorsView.setText("");
+
+        if(parts[0].length() == 0)
+            return;
+        else if(parts.length == 1)
+            visorView.setText(parts[0].substring(0, parts[0].length() - 1));
+        else if(parts.length == 2)
+            visorView.setText(parts[0]);
+        else if(parts.length == 3) {
+            if (parts[2].length() > 1)
+                visorView.setText(parts[0] + ' ' + parts[1] + ' ' + parts[2].substring(0, parts[2].length() - 1));
+            else
+                visorView.setText(parts[0] + ' ' + parts[1]);
+        }
+        else
+            errorsView.setText("Erro desconhecido");
     }
 
     public void clickClearButton(View view){
         EditText visorView = (EditText) findViewById(R.id.visor);
         TextView errorsView = (TextView) findViewById(R.id.errorsText);
-        String text = visorView.getText().toString();
 
         visorView.setText("");
         errorsView.setText("");
